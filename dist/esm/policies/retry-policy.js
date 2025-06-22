ea5f495dcab82fe23d637224dea5c31ebd228529
Bishal Shrestha <bishal.shrestha@outlook.com.au>
2025-06-22 15:27:17 +1000

    removed the builds

--- dist/esm/policies/retry-policy.js
+++
@@ -1,8 +0,0 @@
-/**
- * Default retry condition for transient errors.
- * @param response - Fetch API response object 
- * @returns Whether the request should be retried
- */
-export function shouldRetry(response) {
-  return [502, 503, 504].includes(response.status);
-}
