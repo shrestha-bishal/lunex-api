4ce54a8bf7b2919cf9e34d8ea441b9de6e0850c9
Bishal Shrestha <bishal.shrestha@outlook.com.au>
2025-06-22 15:11:12 +1000

    added named export for restclient

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@
 import RestClient from './rest/RestClient'
 
 export { shouldRetry } from './policies/retry-policy'
+export { default as RestClient } from './rest/RestClient'
 export { default as RestClientOptions } from './rest/RestClientOptions'
 
 export default RestClient;
