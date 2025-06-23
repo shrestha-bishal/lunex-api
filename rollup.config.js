45f6dd8834d17f8c0195129a37a9e067d01db286
shrestha-bishal <bishal.shrestha@outlook.com.au>
2025-06-23 14:07:08 +1000

    added Universal Module Definition (UMD) support

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import typescript from 'rollup-plugin-typescript2';
+import terser from '@rollup/plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/umd/rest-client.umd.js',
+      format: 'umd',
+      name: 'RestClient',
+      sourcemap: true
+    },
+    {
+      file: 'dist/umd/rest-client.umd.min.js',
+      format: 'umd',
+      name: 'RestClient',
+      sourcemap: true,
+      plugins: [terser()]
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.build.json',
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: false,
+          module: 'ESNext'
+        },
+        include: ['src/**/*']
+      }
+    })
+  ]
+};
